40a91be5e1254ffeef31cc33a3f268fff0cb853f
erikvw <ew2789@gmail.com>
2023-03-23 12:29:46 -0500

    dont import model cls

--- edc_refusal/models/__init__.py
+++ edc_refusal/models/__init__.py
@@ -1,2 +1,3 @@
 from .list_models import RefusalReasons
+from .signals import subject_refusal_on_post_delete, subject_refusal_on_post_save
 from .subject_refusal import SubjectRefusal
--- edc_refusal/models/signals.py
+++ edc_refusal/models/signals.py
@@ -3,18 +3,17 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from edc_screening.utils import get_subject_screening_model_cls
 
-from ..utils import get_subject_refusal_model_cls
+from ..utils import get_subject_refusal_model
 
 
 @receiver(
     post_save,
     weak=False,
-    sender=get_subject_refusal_model_cls(),
     dispatch_uid="subject_refusal_on_post_save",
 )
 def subject_refusal_on_post_save(sender, instance, raw, created, **kwargs):
     """Updates `refused` field on SubjectScreening."""
-    if not raw:
+    if not raw and sender._meta.label_lower == get_subject_refusal_model():
         try:
             obj = get_subject_screening_model_cls().objects.get(
                 screening_identifier=instance.screening_identifier
@@ -29,17 +28,17 @@ def subject_refusal_on_post_save(sender, instance, raw, created, **kwargs):
 @receiver(
     post_delete,
     weak=False,
-    sender=get_subject_refusal_model_cls(),
     dispatch_uid="subject_refusal_on_post_delete",
 )
 def subject_refusal_on_post_delete(sender, instance, using, **kwargs):
     """Updates/resets `refused` field on SubjectScreening."""
-    try:
-        obj = get_subject_screening_model_cls().objects.get(
-            screening_identifier=instance.screening_identifier
-        )
-    except ObjectDoesNotExist:
-        pass
-    else:
-        obj.refused = False
-        obj.save(update_fields=["refused"])
+    if sender._meta.label_lower == get_subject_refusal_model():
+        try:
+            obj = get_subject_screening_model_cls().objects.get(
+                screening_identifier=instance.screening_identifier
+            )
+        except ObjectDoesNotExist:
+            pass
+        else:
+            obj.refused = False
+            obj.save(update_fields=["refused"])
