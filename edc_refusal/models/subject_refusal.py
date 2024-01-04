a3a09c8889226eee371371a1a78cea0dc7c8fab4
erikvw <ew2789@gmail.com>
2024-01-03 19:28:57 -0600

    update edc_site path

--- edc_refusal/models/subject_refusal.py
+++ edc_refusal/models/subject_refusal.py
@@ -1,5 +1,6 @@
 from edc_model.models import BaseUuidModel, HistoricalRecords
-from edc_sites.models import CurrentSiteManager, SiteModelMixin
+from edc_sites.managers import CurrentSiteManager
+from edc_sites.model_mixins import SiteModelMixin
 
 from ..managers import SubjectRefusalManager
 from ..model_mixins import SubjectRefusalModelMixin
