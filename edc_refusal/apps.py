a0115c9ae7fabbbb29052e3a8a7d36d7f0f7123b
erikvw <ew2789@gmail.com>
2024-11-04 06:00:33 -0600

    do not include on administration page

--- edc_refusal/apps.py
+++ edc_refusal/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig as DjangoApponfig
 class AppConfig(DjangoApponfig):
     name = "edc_refusal"
     verbose_name = "Edc Refusal"
-    include_in_administration_section = True
+    include_in_administration_section = False
     has_exportable_data = True
     default_auto_field = "django.db.models.BigAutoField"
